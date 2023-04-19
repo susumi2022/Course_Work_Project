400346fd2fb1ee42ad1ae9001392df415cfaa468
Udaya Manage <um22aab@herts.ac.uk>
2023-04-19 21:57:04 +0100

    Add validations

--- src/com/wfc/Timetables.java
+++ src/com/wfc/Timetables.java
@@ -7,47 +7,79 @@ import java.util.Scanner;
 public class Timetables {
     Scanner reader = new Scanner(System.in);
 
-    public boolean ViewTimetableMenu(int menuId) {
+    Bookings booking = new Bookings();
+    public void ViewTimetableMenu(int menuId) {
         System.out.println("1.1) Day wise timetable (1.1)");
         System.out.println("1.2) Type wise timetable (1.2)");
 
         System.out.println("Which timetable do you want to see?\n");
         double timeTableId = reader.nextDouble();
+        String timeTableType = "";
 
         if (timeTableId == 1.1) {
             System.out.println("Saturday (Sat) or Sunday (Sun)?\n");
-            ViewTimetableByDays(reader.nextLine().toUpperCase());
-            return false;
+            timeTableType = reader.nextLine().toUpperCase();
+            if(timeTableType !=null || timeTableType != ""){
+                ViewTimetableByDays(reader.nextLine().toUpperCase());
+            } else {
+                System.out.println("Invalid selection\n");
+            }
+            //return false;
         } else if (timeTableId == 1.2) {
             System.out.println("Select which type - timetable? (S-SPAIN / Y-YOGA / Z-ZUMBA / B-BODY SCULPT)\n");
-            ViewTimetableByTypes(reader.nextLine().toUpperCase());
-            return false;
+            timeTableType = reader.nextLine().toUpperCase();
+            if(timeTableType !=null || timeTableType != ""){
+                ViewTimetableByTypes(reader.nextLine().toUpperCase());
+            } else {
+                System.out.println("Invalid selection\n");
+            }
+            //return false;
         } else {
-            System.out.println("Entered menu number is invalid.Please enter the correct manu number.....\n");
-            timeTableId = reader.nextFloat();
-            return true;
+            System.out.println("Entered menu number is invalid.\n");
+            MainMenu m = new MainMenu();
+            m.getMainMenus();
         }
 
     }
     public void ViewTimetableByDays(String day){
         try {
+            //System.out.println(day.toUpperCase());
             String fileName = "";
-            if(day.toUpperCase() == "SATURDAY"){
+            if(day.toUpperCase().equals("SAT")){
                 fileName = "src/Saturday_Timetable.txt";
             }
-            else if(day.toUpperCase() == "SATURDAY"){
+            else if(day.toUpperCase().equals("SUN")){
                 fileName = "src/Sunday_Timetable.txt";
             }
             else {
-                System.out.println("Invalid.");
+                System.out.println("Invalid");
+                MainMenu m = new MainMenu();
+                m.getMainMenus();
             }
-            File myObj = new File(fileName);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+
+            if(fileName != ""){
+                File myObj = new File(fileName);
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+
+                System.out.println("Would you like to booking your lesson (Y/N)?");
+                String result = reader.nextLine();
+                if(result.toUpperCase().equals("Y")){
+                    booking.setBookingsDetails();
+                } else if (result.toUpperCase().equals("N")) {
+                    MainMenu mainMenu1 = new MainMenu();
+                    mainMenu1.getMainMenus();
+                } else {
+                    System.out.println("Invalid.");
+                    MainMenu m = new MainMenu();
+                    m.getMainMenus();
+                }
             }
-            myReader.close();
+
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -57,26 +89,44 @@ public class Timetables {
     public void ViewTimetableByTypes(String type){
         try {
             String fileName = "";
-            if(type.toUpperCase() == "S"){
+            if(type.toUpperCase().equals("S")){
                 fileName = "src/Spain_timetable.txt";
-            }else if(type.toUpperCase() == "Y"){
+            }else if(type.toUpperCase().equals("Y")){
                 fileName = "src/Yoga_timetable.txt";
-            } else if(type.toUpperCase() == "Z"){
+            } else if(type.toUpperCase().equals("Z")){
                 fileName = "src/Zumba_timetable.txt";
-            } else if(type.toUpperCase() == "B"){
+            } else if(type.toUpperCase().equals("B")){
                 fileName = "src/BodySculpt_timetable.txt";
             }
             else {
                 System.out.println("Invalid.");
+                MainMenu m = new MainMenu();
+                m.getMainMenus();
             }
 
-            File myObj = new File(fileName);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+            if(fileName != "") {
+                File myObj = new File(fileName);
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+
+                System.out.println("Would you like to booking your lesson (Y/N)?");
+                String responce = reader.nextLine();
+                if(responce.toUpperCase().equals("Y")){
+                    booking.setBookingsDetails();
+                } else if (responce.toUpperCase().equals("N")) {
+                    MainMenu mainMenu1 = new MainMenu();
+                    mainMenu1.getMainMenus();
+                } else {
+                    System.out.println("Invalid.");
+                    MainMenu m = new MainMenu();
+                    m.getMainMenus();
+                }
+
             }
-            myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
